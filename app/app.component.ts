cf8bdd9f9616a058f7336186ae18bcfe16726e3c
Valeriy Belich <vbelich@amtoss.com.ua>
2017-09-01 14:22:03 +0300

    initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+selector: 'my-app',
+template: `<p>Количество кликов {{count}}</p>
+          <button (click)="increase()">Click</button>`
+})
+export class AppComponent { 
+count: number=0;
+increase() : void {
+  this.count++;
+}
+}
